9fea8d3653dc63c6d68b2d7d0bf82bf8e1df4191
kanekomasanori <alley.cats.ml(a)gmail.com>
2015-06-08 09:09:39 +0900

    development環境の通常ログとエラーログを分ける対応

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.logger = Logger.new('log/development.log', 'daily')
+  config.error_logger = Logger.new('log/development_error.log', 'daily')
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
